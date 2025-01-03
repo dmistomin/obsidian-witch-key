9b49424b76aec704223f04a293c6e0296db2b5ed
Dmitriy Istomin <distomin+git@pm.me>
2025-01-03 19:10:55 +0900

    remove some demo code; add basic vim mode detection to plugin

--- main.ts
+++ main.ts
@@ -1,89 +1,119 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownView, Plugin, PluginSettingTab, Setting } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+enum VimMode {
+	None,
+	Normal,
+	Insert,
+	Visual,
+	Replace,
+}
 
 interface WitchKeyPluginSettings {
 	mySetting: string;
 }
 
 const DEFAULT_SETTINGS: WitchKeyPluginSettings = {
-	mySetting: 'default'
-}
+	mySetting: "default",
+};
 
 export default class WitchKeyPlugin extends Plugin {
 	settings: WitchKeyPluginSettings;
 
+	private codeMirrorVim: CodeMirror.Editor;
+	private vimMode: VimMode = VimMode.None;
+
+	private getActiveView(): MarkdownView | null {
+		return this.app.workspace.getActiveViewOfType(MarkdownView);
+	}
+
+	private getCodeMirrorEditor(view: MarkdownView): CodeMirror.Editor {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const codeMirrorEditor = (view as any).editMode?.editor?.cm?.cm;
+		return codeMirrorEditor;
+	}
+
+	private syncVimModeOnChange = (ev: {
+		mode: "insert" | "normal" | "visual" | "replace";
+	}): void => {
+		console.log("syncVimModeOnChange(), event:", ev);
+
+		if (!ev) {
+			return;
+		}
+
+		switch (ev.mode) {
+			case "insert":
+				this.vimMode = VimMode.Insert;
+				break;
+			case "normal":
+				this.vimMode = VimMode.Normal;
+				break;
+			case "visual":
+				this.vimMode = VimMode.Visual;
+				break;
+			case "replace":
+				this.vimMode = VimMode.Replace;
+				break;
+			default:
+				this.vimMode = VimMode.None;
+				break;
+		}
+
+		console.log("vim mode set: ", this.vimMode);
+	};
+
+	private setupVimHandlers() {
+		const view = this.getActiveView();
+		if (view) {
+			this.vimMode = VimMode.None;
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			const cmEditor = this.getCodeMirrorEditor(view) as any;
+			cmEditor.off("vim-mode-change", this.syncVimModeOnChange);
+			cmEditor.on("vim-mode-change", this.syncVimModeOnChange);
+		}
+	}
+
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Witch Key Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('Hello world!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new WitchKeySettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		const view = this.getActiveView();
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		if (!view) {
+			// This means that no Markdown view is available on plugin load.
+			// We add handlers to wait until a window is open.
+
+			this.app.workspace.on("window-open", () => {
+				console.log("window-open");
+				this.setupVimHandlers();
+			});
+			this.app.workspace.on("window-close", () => {
+				console.log("window-close");
+				this.setupVimHandlers();
+			});
+			this.app.workspace.on("active-leaf-change", () => {
+				console.log("active-leaf-change");
+				this.setupVimHandlers();
+			});
+			this.app.workspace.on("file-open", () => {
+				console.log("file-open");
+				this.setupVimHandlers();
+			});
+		}
+
+		this.setupVimHandlers();
 	}
 
-	onunload() {
-
-	}
+	onunload() { }
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData(),
+		);
 	}
 
 	async saveSettings() {
@@ -91,23 +121,7 @@ export default class WitchKeyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class WitchKeySettingTab extends PluginSettingTab {
 	plugin: WitchKeyPlugin;
 
 	constructor(app: App, plugin: WitchKeyPlugin) {
@@ -116,19 +130,21 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					}),
+			);
 	}
 }
